f157aeb8a719fe4fb92826bfa2fbfc4fa7819987
kuburmuaz <kuburmuaz>
2023-07-23 16:16:02 -0500

    Initial commit

---
+++ src/main/java/com/example/jenkinseg/JenkinsEgApplication.java
@@ -0,0 +1,10 @@
+package com.example.jenkinseg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsEgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsEgApplication.class, args
---
+++ src/test/java/com/example/jenkinseg/JenkinsEgApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.jenkinseg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsEgApplicationTests {
+
+    @Test
+    void contextLoads() {
